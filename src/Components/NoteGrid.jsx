9cca1e4d0107df714e02dfe381bcc4d04855ed2b
dharani2255 <58243165+dharani2255@users.noreply.github.com>
2026-08-15 17:40:06 +0530

    Update NoteGrid

---
+++ src/Components/NoteGrid.jsx
@@ -0,0 +1,30 @@
+import NoteCard from "./NoteCard"
+
+function NoteGrid({notes,setEdit,deleteNote,setIsOpen}){
+    const openCreateNote = () => {
+        setEdit(null);
+        setIsOpen(true);
+    };
+    return (
+       <div class="grid">
+ 
+           {
+            notes.length==0?(
+                <div
+                className="note blank-note"
+                onClick={() => setIsOpen(true)}
+                style={{ backgroundColor: "#F5F5DC", cursor: "pointer" }}
+                >
+                <p onClick={openCreateNote} style={{ color: "#aaa", textAlign: "center", marginTop: "auto", marginBottom: "auto" }}>
+                    + Click to add a noteg
+                </p>
+                </div>  
+            ):
+            notes.map(note=>
+                <NoteCard setEdit={setEdit} deleteNote={deleteNote} {...note}/>
+            )
+           }
+       </div>
+    )
+}
+export default NoteGrid
